bbe0d143bcc73901907f71645ebdb8b49316d734
Jeff Macaluso <samacaluso@vassar.edu>
2016-04-18 11:09:09 -0400

    Initial Commit
    Art Department at Vassar

---
+++ assets/config.php
@@ -0,0 +1,64 @@
+<?php
+return 
+array(
+
+	'useLocalNav' => false,
+	'siteID' => '47896',
+	// How to Load assets
+		'minifyCSS' => false,
+		'minifyJS' => true,
+			
+	// Fonts
+
+		'typekitID' => 'dgd4gjk',
+		//'typekitFade' => true,
+		//'typekitFadeDuration' => 0,
+		//'typekitSelector' => 'body>*',
+		
+		//'siteDesignType'=>'office',
+		
+	// Name of the site used in masthead, footer, etc.
+	 'siteName' => 'Art Department',
+		
+	//Javascript
+		//'loadGA' => "1",
+		//'loadTabs' => true,
+		//'loadGalleria' => false,
+		//'versionjQuery' => '2.1.0',//1.11.1',
+	// CMS site ID
+	// Computing and information services
+	// 3068
+	// College Website
+	// 2890
+	// Anthropology Dept
+	// 2779
+	// French
+	// 47348
+	// Wonen's Center
+	// 8249
+	
+		'starter' => true,
+		'siteID' => '47896',//'19295',//'',//3855',// '2761',//1833',//2761',//19134',//1833',//8249',//47348',
+			
+	// Should be either department or office, not both
+	// Identify as a department site 
+	//	'siteCode' => 'bioc',
+			
+	// Identify as an administrative office site
+	//	'siteType' => 'department',
+		
+	// Contact info
+	//	'officeBoxNumber' => 274,
+	//	'officeLocationBuilding' => ' New England Building', 
+	//	'officeLocationRoom' => '208B', 
+	//	'officeURL' => '', 
+	//	'officePhone' => '5472',
+	//	'officeFax' => '7204', 
+	//	'officeEmail' => 'urbanstudies', 
+		
+		'dev' => 'new',
+		//'phpErrorReporting' => E_ALL,
+		//'debug' => true,
+		//'locallyHostedCSS' => 'http://localhost:8888/web/sites/people/rayschwartz/gamut-2.4/assets/css/test.css',
+		
+);
---
+++ assets/plugins/Galleria/config.php
@@ -0,0 +1,284 @@
+<?php namespace gamut;/*?><root>*/?>
+<?php
+return 
+    array(
+array(
+ 'galleryHome' => '',
+'id' => 'history',
+'idleMode' => false,
+'imageCrop' => 'height',
+'modal' => true,
+'thumbnails' => false,
+'showImagenav' => true,
+'showCounter' => true,
+'showInfo' => true,
+'slidesLocation' => '',
+
+'slides' => array (array('description'=><<<HTML
+Anon., The original Vassar College Art Gallery in Main Building, c. 1864. Frances Lehman Loeb Art Center, Vassar College
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/history/vassar_gallery_large.jpg'),array('description'=><<<HTML
+Professor Henry van Ingen in front of the cast of Ghiberti&apos;s Gates of Paradise, Special Collections 08.3.22
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/history/van_ingen_large.jpg'),array('description'=><<<HTML
+Gallery of plaster casts in Taylor Hall, c. 1915. Special Collections Ph.f5.21
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/history/avery_casts_large.jpg'),array('description'=><<<HTML
+Professor Oliver S. Tonks, Special Collections Pf.f8.79
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/history/tonks_large.jpg'),array('description'=><<<HTML
+Professor Agnes Rindge, c. 1928, by George Platt Lynes (photo: Susan Kuretsky)
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/history/lynes_large.jpg'),array('description'=><<<HTML
+Professor Leila Cook Barber, c. 1935 by George Platt Lynes (photo: estate of Pamela Askew)
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/history/barber_large.jpg'),array('description'=><<<HTML
+Professor Clarence Chatterton seated outdoors, Special Collections
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/history/chatterton_large.jpg'),array('description'=><<<HTML
+Life Drawing, circa 1930s. Ely Studios. Special collections
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/history/chatterton_nude_large.jpg'),array('description'=><<<HTML
+Professor Richard Krautheimer at his Desk. c. 1938. Special Collections Ph.f7.21
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/history/krautheimer_large.jpg'),array('description'=><<<HTML
+Professor Adolf Katzenellenbogen, Special Collections
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/history/katzenellenbogen_large.jpg'),array('description'=><<<HTML
+Anne Cleveland, "An Art History Lecturer at Vassar," from The Educated Woman in Cartoon and Caption by Anne Cleveland and Jean Anderson, New York, 1960.
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/history/cleveland_cartoon_large.jpg'),array('description'=><<<HTML
+Professor Pamela Askew working with slides, 1985. Special Collections
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/history/askew_large.jpg'),array('description'=><<<HTML
+Professor Eugene Carroll, 1983 by Andrew Cooper &apos;83 (photo: Eugene Carroll)
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/history/cooper_large.jpg'),array('description'=><<<HTML
+Professor Christine Havelock, Special Collections
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/history/havelock_large.jpg'),array('description'=><<<HTML
+Jeh Johnson, consulting with an architecture student over a model. Special Collections
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/history/johnson_large.jpg'),array('description'=><<<HTML
+Professor Lewis Rubenstein at easel with a student, Special Collections
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/history/rubenstein_large.jpg'),array('description'=><<<HTML
+Philip Pearlstein, Portrait of Linda Nochlin and Richard Pommer, 1968 (collection (Linda Nochlin Pommer)
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/history/pearlsteibn_portrait_large.jpg'),array('description'=><<<HTML
+Working with mounted photographs in Taylor Hall (Professor Richard Pommer with a Student). Special Collections
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/history/po_large.jpg'),array('description'=><<<HTML
+A 1959 class with Linda Nochlin, who became famous for her groundbreaking 1971 essay, "Why Have There Been No Great Women Artists?"
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/history/nochlin_large.jpg'),),),array(
+ 'galleryHome' => '',
+'id' => 'homepage',
+'idleMode' => false,
+'imageCrop' => 'height',
+'modal' => true,
+'thumbnails' => false,
+'showImagenav' => true,
+'showCounter' => true,
+'showInfo' => true,
+'slidesLocation' => '',
+'autoplay'=>true,
+'slides' => array (array('description'=><<<HTML
+Studio art critique
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/homepage/_dsc3011_1720.jpg'),array('description'=><<<HTML
+On the scaffolding at Bourges Cathedral
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/homepage/img_3433_1280.jpg'),array('description'=><<<HTML
+Opening in the Loeb Art Center
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/homepage/fllac-opening_1720.jpg'),array('description'=><<<HTML
+Marco Maggi exhibition in the Loeb Art Center
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/homepage/maggi-1_1017.jpg'),array('description'=><<<HTML
+Professor Andrew Tallon talking with campus visitors in Taylor 103, the department&apos;s largest and most historic lecture hall.
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/homepage/0152_13_10_-KF_0025-copy.jpg'),array('description'=><<<HTML
+In Special Collections with a Book of Hours from the Medieval era
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/homepage/p1240110_1720.jpg'),array('description'=><<<HTML
+Students looking at a 3D representation during an Art 105-106 lecture in Taylor Hall
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/homepage/_DSC8084.jpg'),array('description'=><<<HTML
+Vassar students on field trip to Kykuit
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/homepage/ye-students-at-kykuit-3_1720.jpg'),array('description'=><<<HTML
+Blueprint of first floor of Taylor Hall, home of the Art Department.
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/homepage/U0.jpg'),array('description'=><<<HTML
+Prof. Harry Roseman and a student working on North Entrance, a site-specific installation created for the Thompson Library.
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/homepage/0041_12_07_MEZ-1035.jpg'),array('description'=><<<HTML
+The Art Library, a modernist interior designed in 1937 by John McAndrew
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/homepage/0104_14_10_KR_0042.jpg'),),),array(
+ 'galleryHome' => '',
+'id' => 'student-work',
+'idleMode' => false,
+'imageCrop' => 'height',
+'modal' => true,
+'thumbnails' => false,
+'showImagenav' => true,
+'showCounter' => true,
+'showInfo' => true,
+'slidesLocation' => '',
+
+'slides' => array (array('description'=><<<HTML
+Student artwork
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/student-work/_dsc3039_hr.jpg'),array('description'=><<<HTML
+Student artwork
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/student-work/_dsc3082_hr.jpg'),array('description'=><<<HTML
+Student artwork
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/student-work/_dsc3071_hr.jpg'),array('description'=><<<HTML
+Student artwork
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/student-work/img_2197_hr.jpg'),array('description'=><<<HTML
+Student artwork
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/student-work/img_0817_hr.jpg'),array('description'=><<<HTML
+Student artwork
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/student-work/img_1582_hr.jpg'),array('description'=><<<HTML
+Student artwork
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/student-work/img_0932_hr.jpg'),array('description'=><<<HTML
+Student artwork
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/student-work/img_1180_hr.jpg'),array('description'=><<<HTML
+Student artwork
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/student-work/img_5747_hr.jpg'),array('description'=><<<HTML
+Student artwork
+HTML
+,'title'=><<<HTML
+
+HTML
+,'url'=>'/assets/images/student-work/img_5753_hr.jpg'),),),
+    );
+?>
+<?php // </root>
